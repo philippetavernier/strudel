7a9011ba778bdc50a5e76c6c5ed51aaf32b268b3
philippetavernier <philippe.tavernier@gmail.com>
2025-11-13 10:04:10 +0100

    Create drum-and.js

---
+++ drum-and.js
@@ -0,0 +1,32 @@
+
+setcpm(160/4)
+//setcpm(90/4)
+
+
+bass : note(`<
+[e5 [b4 c5] d5 [c5 b4]]
+[a4 [a4 c5] e5 [d5 c5]]
+[b4 [~ c5] d5 e5]
+[c5 a4 a4 ~]
+[[~ d5] [~ f5] a5 [g5 f5]]
+[e5 [~ c5] e5 [d5 c5]]
+[b4 [b4 c5] d5 e5]
+[c5 a4 a4 ~]
+,
+[[e2 e3]*4]
+[[a2 a3]*4]
+[[g#2 g#3]*2 [e2 e3]*2]
+[a2 a3 a2 a3 a2 a3 b1 c2]
+[[d2 d3]*4]
+[[c2 c3]*4]
+[[b1 b2]*2 [e2 e3]*2]
+[[a1 a2]*4]
+>`).sound("piano")._punchcard()
+drum : sound(`
+[oh -  -  - ] [-  -  -  - ] [-  -  -  - ] [-  -  -  - ],
+[hh hh -  - ] [hh -  hh - ] [hh -  hh - ] [hh -  hh - ],
+[-  - cp  - ] [-  -  cp - ] [-  -  cp - ] [-  -  cp - ],
+[bd -  -  - ] [bd  -  -  -] [bd -  -  - ] [-  -  -  bd]
+`).bank("RolandTR909")._punchcard({width: 600})
+
+ 
